76160e095d2f40cddd4c2a841f949ad8ebc73eef
Skyler Emery <2get2skyler@gmail.com>
2024-03-27 19:23:09 -0500

    close to all final touch ups before database

--- SignIn.py
+++ SignIn.py
@@ -11,28 +11,36 @@ root.geometry("500x250+10+10")
 class SignIn:
     def __init__(self,master):
 
-        #EMail
-        self.label1 = ttk.Label(master)
-        self.label1.config(text = "Email: ")
-        self.label1.grid(row=0,column=0)
+        #Email
+        self.emailLabel = ttk.Label(master)
+        self.emailLabel.config(text = "Email: ")
+        self.emailLabel.grid(row=0,column=0)
 
-        self.entry1 = ttk.Entry(master)
-        self.entry1.config()
-        self.entry1.grid(row=0,column=1)
+        self.emailEntry = ttk.Entry(master)
+        self.emailEntry.config()
+        self.emailEntry.grid(row=0,column=1)
+
+        self.emailMessage = ttk.Label(master)
+        self.emailMessage.config()
+        self.emailMessage.grid(row=0,column=2)
 
         #Password
-        self.label2 = ttk.Label(master)
-        self.label2.config(text = "Password: ")
-        self.label2.grid(row=1,column=0)
+        self.passwordLabel = ttk.Label(master)
+        self.passwordLabel.config(text = "Password: ")
+        self.passwordLabel.grid(row=1,column=0)
 
-        self.entry2 = ttk.Entry(master)
-        self.entry2.config()
-        self.entry2.grid(row=1,column=1)
+        self.passwordEntry = ttk.Entry(master)
+        self.passwordEntry.config()
+        self.passwordEntry.grid(row=1,column=1)
+
+        self.passwordMessage = ttk.Label(master)
+        self.passwordMessage.config()
+        self.passwordMessage.grid(row=1,column=2)
 
         #Sign In
-        self.button1 = ttk.Button(master)
-        self.button1.config(text = "Sign In")
-        self.button1.grid(row=2,column=1)
+        self.signIn = ttk.Button(master)
+        self.signIn.config(text = "Sign In",command=lambda: [self.emailGrab(),self.passwordGrab()])
+        self.signIn.grid(row=2,column=1)
 
     #Already Have An Account
         self.button2 = ttk.Button(master)
@@ -43,6 +51,24 @@ class SignIn:
     def openSignUpPage(self):
         subprocess.Popen(['python', 'SignUp.py'])
         root.destroy()
+
+    #grabs email
+    def emailGrab(self):
+        email = self.emailEntry.get()
+
+        if '@' not in email:
+            self.emailMessage.config(text = "Invalid email format: '@' symbol is missing")
+        else:
+            self.emailMessage.config(text = "Email is valid")
+
+    #grabs password
+    def passwordGrab(self):
+        password = self.passwordEntry.get()
+
+        if password == "":
+            self.passwordMessage.config(text = "Error, no password provided")
+        else:
+            self.passwordMessage.config(text = "")
 
 
 
